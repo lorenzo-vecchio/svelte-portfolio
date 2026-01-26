057b8b517ad85709be2c90456be117732cb46da9
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2026-01-26 22:05:44 +0100

    add mobile menu functionality and integrate footer component

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -3,6 +3,8 @@
   import { navItems } from "$lib/conf";
   import { onMount } from "svelte";
   import ThemeButton from "./theme-button.svelte";
+  import { Menu, X } from "@lucide/svelte";
+  import Footer from "$lib/sections/footer.svelte";
 
   let isScrolled = $state(false);
   let isMobileMenuOpen = $state(false);
@@ -21,12 +23,14 @@
 <nav
   class={[
     "z-50 fixed top-0 left-0 right-0 transition-all duration-300",
-    isScrolled ? "bg-background/70 backdrop-blur-md border-b border-border" : "",
+    isScrolled
+      ? "bg-background/70 backdrop-blur-md border-b border-border"
+      : "",
   ]}
 >
   <div class="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
     <Button href="/" variant="ghost" class="text-xl font-bold text-primary">
-        <span class="font-black">LGV</span>
+      <span class="font-black">LGV</span>
     </Button>
 
     <!-- Desktop Navigation -->
@@ -38,5 +42,41 @@
       {/each}
       <ThemeButton />
     </div>
+
+    <!-- Mobile Menu Button -->
+  <button
+    class="md:hidden inline-flex items-center justify-center rounded-md p-2 hover:bg-muted"
+    onclick={() => (isMobileMenuOpen = !isMobileMenuOpen)}
+  >
+    {#if isMobileMenuOpen}
+      <X class="w-5 h-5" />
+    {:else}
+      <Menu class="w-5 h-5" />
+    {/if}
+  </button>
   </div>
+
+  
+
+  <!-- Mobile Menu -->
+  {#if isMobileMenuOpen}
+    <div class="md:hidden bg-background border-b border-border h-screen w-screen flex flex-col justify-between items-center">
+      <div></div>
+      <div class="space-y-2 flex flex-col justify-center items-center">
+        {#each navItems as item}
+          <a
+            href={item.href}
+            onclick={() => (isMobileMenuOpen = false)}
+            class="block py-2 text-2xl"
+          >
+            {item.label}
+          </a>
+        {/each}
+      </div>
+      <ThemeButton />
+      <div class="mb-14">
+        <Footer />
+      </div>
+    </div>
+  {/if}
 </nav>
